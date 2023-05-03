7f022fb0baef66b0741af52f07e29e86b514921a
Rahees <ahdrahees.dev@gmail.com>
2023-05-03 23:58:59 +0530

    Initial commit
     Added <Header> & <LotteryEntrance> components

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+// imports work with our front-ends
+// require doesnot
+// nodejs != javascript
+// backend-js is little different from frontend-js
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "next/font/google"
+import styles from "@/styles/Home.module.css"
+// import ManualHeader from "@/components/ManualHeader"
+import Header from "@/components/Header"
+import LotteryEntrance from "@/components/LotteryEntarnce"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Smart Contract Lottery</title>
+                <meta name="description" content="Our smartcontract lottery" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {/* header / connect button / nav bar */}
+            <div>
+                {/* <ManualHeader /> */}
+                <Header />
+                <LotteryEntrance />
+            </div>
+        </>
+    )
+}
