dbd6a0d27adb44238a37dcbb67c8853ed8927cc9
Rahees <ahdrahees.dev@gmail.com>
2023-05-08 12:16:58 +0530

    Added some  Styling

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,11 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <div>
-            Decentralized Lottery
-            <ConnectButton moralisAuth={false} />
+        <div className="p-5 border-b-4 flex flex-row">
+            <h1 className="px-4 py-4 font-bold text-3xl">Decentralized Lottery</h1>
+            <div className="ml-auto py-4 px-2">
+                <ConnectButton moralisAuth={false} />
+            </div>
         </div>
     )
 }
