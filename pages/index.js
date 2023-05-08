4fb82a52dfbb78a80bfd0efa779b7adb07d1c1e7
Rahees <ahdrahees.dev@gmail.com>
2023-05-08 12:24:25 +0530

    Modified

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,10 @@
 // nodejs != javascript
 // backend-js is little different from frontend-js
 import Head from "next/head"
-import Image from "next/image"
-import { Inter } from "next/font/google"
-import styles from "@/styles/Home.module.css"
+
 // import ManualHeader from "@/components/ManualHeader"
 import Header from "@/components/Header"
 import LotteryEntrance from "@/components/LotteryEntarnce"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
     return (
